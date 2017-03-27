4e359ba845bad36bfed093a27624b5103525ae60
YunSeok[Andy] NA <alinghi@kaist.ac.kr>
2017-03-27 09:51:22 +0900

    Implementation of Network Function

--- alinghi_worm.c
+++ alinghi_worm.c
@@ -6,6 +6,98 @@
 
 //Preprocessor for IPaddress
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#define MAX_BUF   (1024)
+
+int sockfd;
+
+//Network Connection
+//to port 2323
+//return fd
+int connect2323(uint32_t ipAddress){
+	struct sockaddr_in servaddr;
+	uint32_t actual_addr;
+	struct sockaddr_in temp;
+	int count=0;
+	char* ipAddr;
+
+	//socket
+	if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
+	{
+		printf("socket error\n");
+		return 0;
+	}
+
+	//init server address
+	memset( &servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_port=htons(2323);
+	actual_addr=htonl(ipAddress);
+	temp.sin_addr.s_addr=actual_addr;
+	ipAddr=inet_ntoa(temp.sin_addr);
+	//connect
+	printf("ip : %s \n",ipAddr);
+	if(inet_pton(AF_INET,ipAddr,&servaddr.sin_addr)<=0)
+		printf("inet_pton error\n");
+	if(connect(sockfd,(struct sockaddr*) &servaddr, sizeof(servaddr))<0)
+		printf("connect error\n");
+	return 1;
+}
+
+//Actual Worm
+int worm(uint32_t ipAddress){
+	int count=0;
+	int r;
+	char buf[MAX_BUF];
+	char input[MAX_BUF];
+	//Make socket file descriptor
+	if(!connect2323(ipAddress))
+		//In case of fail
+		return 0;
+	//write
+	//write
+	while(count<2){
+		r = readSock( sockfd, buf, MAX_BUF );
+		if ( r == -1 ) return -1;
+		printf("%s",buf);
+   		fgets(input,MAX_BUF,stdin);
+   		r = send( sockfd, input, strlen( input ), 0 );
+    		if ( r == -1 ) { perror( "send" ); return -1; }
+    		count++;
+	}
+	while(1){
+   		fgets(input,MAX_BUF,stdin);
+   		if(input[0]=='\n')
+   		{
+   			printf("new\n");
+   			continue;
+   		}
+   		r = send( sockfd, input, strlen( input ), 0 );
+    		if ( r == -1 ) { perror( "send" ); return -1; }
+    		printf("get the command\n");
+
+		r = readSock( sockfd, buf, MAX_BUF );
+		if ( r == -1 ) return -1;
+		printf("%s\n",buf);		
+	}
+	//read
+	//close
+	close(sockfd);
+}
+
+int readSock( int sockfd, char* buf, size_t bufLen )
+{
+    int r;
+
+    r = recv( sockfd, buf, bufLen - 1, 0 );
+    if ( r == -1 ) { perror( "recv" ); return -1; }
+    buf[r - 1] = '\0';
+    return 0;
+}
 
 
 //In case of usageError
@@ -86,6 +178,8 @@ int main( int argc, char* argv[] )
 	//When range of IP address is wrong
 	if(startAddress>endAddress)
 		inputError();
+
+	worm(returnIPAddr("127.0.0.1"));
 
 	return 0;
 }
